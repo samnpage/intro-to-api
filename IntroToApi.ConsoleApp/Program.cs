32064b39d512fe743099e3063663278eb9c1d12a
Sam Page <samnpage96@gmail.com>
2023-10-12 22:46:42 -0400

    Initial Commit

---
+++ IntroToApi.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToApi.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            HttpResponseMessage response = httpClient.GetAsync("https://swapi.dev/api/people/1").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+            }
+        }
+    }
+}
